574cdf19c407fc9a53222858447730413fb05e94
brandonjolley97 <brandonjolley97@gmail.com>
2015-12-17 09:25:09 -0700

    added some IBActions for different methods of color change

--- StupidiOSProject/ViewController.swift
+++ StupidiOSProject/ViewController.swift
@@ -8,14 +8,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController
+{
+    
+    
+    @IBAction func coloChange(sender: UISwitch)
+    {
+        let newRed = CGFloat(Double(arc4random_uniform(256)) / 255.000 )
+        let newGreen = CGFloat(Double(arc4random_uniform(256)) / 255.000 )
+        let  newBlue = CGFloat(Double(arc4random_uniform(256)) / 255.000 )
+        
+        view.backgroundColor = UIColor(red: newRed, green: newGreen, blue: newBlue, alpha: 1.0)
+    }
+    
+    @IBAction func colorChange(sender: UISlider)
+    {
+        let newRed = CGFloat(Double(arc4random_uniform(256)) / 255.000 )
+        let newGreen = CGFloat(Double(arc4random_uniform(256)) / 255.000 )
+        let  newBlue = CGFloat(Double(arc4random_uniform(256)) / 255.000 )
+        
+        view.backgroundColor = UIColor(red: newRed, green: newGreen, blue: newBlue, alpha: 1.0)
+    }
+    
+    @IBAction func changeColor(sender: UIButton)
+    {
+        let newRed = CGFloat(Double(arc4random_uniform(256)) / 255.000 )
+        let newGreen = CGFloat(Double(arc4random_uniform(256)) / 255.000 )
+        let  newBlue = CGFloat(Double(arc4random_uniform(256)) / 255.000 )
+        
+        view.backgroundColor = UIColor(red: newRed, green: newGreen, blue: newBlue, alpha: 1.0)
+    }
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
+    override func didReceiveMemoryWarning()
+    {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
