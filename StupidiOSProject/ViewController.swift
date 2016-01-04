b0fcec20561da160a3f02d495fcbb8522a24bcc5
brandonjolley97 <brandonjolley97@gmail.com>
2016-01-04 08:29:31 -0700

    changed some methods and method names

--- StupidiOSProject/ViewController.swift
+++ StupidiOSProject/ViewController.swift
@@ -12,25 +12,29 @@ class ViewController: UIViewController
 {
     
     
-    @IBAction func coloChange(sender: UISwitch)
+    
+    @IBAction func hasRed(sender: UISwitch)
     {
-        let newRed = CGFloat(Double(arc4random_uniform(256)) / 255.000 )
-        let newGreen = CGFloat(Double(arc4random_uniform(256)) / 255.000 )
-        let  newBlue = CGFloat(Double(arc4random_uniform(256)) / 255.000 )
-        
-        view.backgroundColor = UIColor(red: newRed, green: newGreen, blue: newBlue, alpha: 1.0)
+        if(sender.on)
+        {
+            view.backgroundColor = UIColor(red: CGFloat(0.999), green: CGFloat(0.134), blue: CGFloat(0.745), alpha: 1.0)
+        }
+        else
+        {
+            view.backgroundColor = UIColor(red: CGFloat(0.001), green: CGFloat(0.134), blue: CGFloat(0.745), alpha: 1.0)
+        }
     }
     
-    @IBAction func colorChange(sender: UISlider)
+    @IBAction func changeRed(sender: UISlider)
     {
-        let newRed = CGFloat(Double(arc4random_uniform(256)) / 255.000 )
-        let newGreen = CGFloat(Double(arc4random_uniform(256)) / 255.000 )
-        let  newBlue = CGFloat(Double(arc4random_uniform(256)) / 255.000 )
         
-        view.backgroundColor = UIColor(red: newRed, green: newGreen, blue: newBlue, alpha: 1.0)
+        let newRed = CGFloat(sender.value)
+        
+        
+        view.backgroundColor = UIColor(red: newRed, green: CGFloat(0.134), blue: CGFloat(0.745), alpha: 1.0)
     }
     
-    @IBAction func changeColor(sender: UIButton)
+    @IBAction func randomColorChange(sender: UIButton)
     {
         let newRed = CGFloat(Double(arc4random_uniform(256)) / 255.000 )
         let newGreen = CGFloat(Double(arc4random_uniform(256)) / 255.000 )
